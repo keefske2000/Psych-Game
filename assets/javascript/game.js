d2079a3d92249c4b275e29af7e1faa3ed9b1cd0f
keefske2000 <keith.w.collins@gmail.com>
2018-11-12 20:59:17 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,23 @@
 
+   <script src="assets/javascript/game.js"></script>
+  
+  
+  // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+   var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+   "t", "u", "v", "w", "x", "y", "Z"];
+
+   // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+   var Wins = 0;
+   var Losses = 0;
+   var GuessesLeft = 10;
+   var YourGuessesSofar = 0;
+
+   // Create variables that hold references to the places in the HTML where we want to display things.
+   var directionsText = document.getElementById("directions-text");
+   var userChoiceText = document.getElementById("Wins");
+   var computerChoiceText = document.getElementById("Losses");
+   var winsText = document.getElementById("GuessesLeft");
+   var lossesText = document.getElementById("YourGuesses");
 
 document.onkeyup = function(event) {
 
